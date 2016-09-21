2bb923e9c04d9acb38c82778f48e9de3075ba714
Eric Fried <efried@us.ibm.com>
2016-09-21 18:18:20 -0500

    Switch hacking.checks from neutron to neutron_lib
    Per armax recommendation in https://review.openstack.org/#/c/374425/,
    moving away from the neutron hacking checks, using neutron_lib hacking
    checks instead.
    
    We need to bypass N530 (direct neutron imports not allowed) for the
    forseeable future.
    
    Change-Id: I05804844445575a55ab33273b88fd3a5ff9df179
    Closes-Bug: #1626298

--- networking_powervm/hacking/checks.py
+++ networking_powervm/hacking/checks.py
@@ -14,7 +14,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from neutron.hacking import checks
+from neutron_lib.hacking import checks
 
 
 def no_log_warn(logical_line):
